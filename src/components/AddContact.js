aa4a2700272e5466baa2f43396252f139fe71576
abid0801 <abid.hassan@northsouth.edu>
2021-06-06 13:52:22 +0600

    Add conatcts Updated

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { uuid } from 'uuidv4';
 const AddContact = () => {
